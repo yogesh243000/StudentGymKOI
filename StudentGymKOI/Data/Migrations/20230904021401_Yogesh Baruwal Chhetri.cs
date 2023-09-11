2643ba68aacdbdd2475f8dcb1ee19a4e084159e3
yogesh243000 <140988573+yogesh243000@users.noreply.github.com>
2023-09-11 11:07:03 +1000

    input

---
+++ StudentGymKOI/Data/Migrations/20230904021401_Yogesh Baruwal Chhetri.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StudentGymKOI.Data.Migrations
+{
+    public partial class YogeshBaruwalChhetri : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
